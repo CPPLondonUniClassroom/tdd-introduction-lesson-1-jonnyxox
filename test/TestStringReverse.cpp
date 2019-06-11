5ae889c8cf345ab5b4af7cce0d0233edadfefbd8
Jon <jon.jedjedi@gmail.com>
2019-06-11 20:37:18 +0100

    first test

--- test/TestStringReverse.cpp
+++ test/TestStringReverse.cpp
@@ -11,5 +11,18 @@ TEST_CASE("A default constructed StringReverse is empty", "[stringreverse]") {
 
 TEST_CASE("A StringReverse constructed with 'Hello World' becomes 'dlroW olleH'", "[stringreverse]") {
     const StringReverse s{"Hello World"};
-    //TODO: test it
+    CHECK_FALSE(s == "Hello World");
+    CHECK (s == "dlroW olleH");
+    CHECK_FALSE(s.empty());
+}
+
+TEST_CASE("We receive ", "[stringreverse]") {
+    const StringReverse s{"LondonUni"};
+    std::string out = s;
+    CHECK(out == "inUnodnoL");
+}
+
+TEST_CASE("we can move ", "[stringreverse]") {
+    
+
 }
